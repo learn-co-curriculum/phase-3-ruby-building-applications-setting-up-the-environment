9eaf56b483d1a31d5bb1120dcec1a013e84e0b86
maxwellbenton <maxwellbenton@gmail.com>
2019-08-20 08:44:34 -0400

    adds example files

---
+++ config/environment.rb
@@ -0,0 +1,4 @@
+# Require statements should go here.
+
+# Then any files that need to load the files required here can
+# require THIS file, environment.rb, and get access to everything
---
+++ lib/garden.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class Garden
+  attr_accessor :name
+
+  def initialize(name:)
+    @name = name
+  end
+
+  def plants
+    Plant.all.select do |plant|
+      plant.garden == self
+    end
+  end
+end
