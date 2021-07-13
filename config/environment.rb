3551153dfe73b977710efabb423b3d10883d11ef
ihollander <ianholla@gmail.com>
2021-07-13 07:17:06 -0400

    Added solution

--- config/environment.rb
+++ config/environment.rb
@@ -2,3 +2,5 @@
 
 # Then any files that need to load the files required here can
 # require THIS file, environment.rb, and get access to everything
+require_relative '../lib/garden'
+require_relative '../lib/plant'
